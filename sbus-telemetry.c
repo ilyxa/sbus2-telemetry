3baf402b2b5e99897be16829fe098ebb9ed2fe46
Ilya V Tyshchenko <ilyxa@nest.org.ru>
2017-04-03 16:38:10 +0300

    slot table

--- sbus-telemetry.c
+++ sbus-telemetry.c
@@ -4,25 +4,50 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/cm3/nvic.h>
 
-//~ #include <stdio.h>
-
 #define SBUS_PORT USART1
 #define CONSOLE_PORT USART3
 #define LED0 GPIO3 // PB3
 #define led0_toggle 	gpio_toggle(GPIOB, LED0);
 #define delay800  for (int i = 0; i < 800000; i++) __asm__("nop");
+#ifdef ENABLE_SEMIHOSTING
+#define DEBUG
+#endif
+
+#ifdef DEBUG
+#include <stdio.h>
+#endif
+
+//~ static int rpm = 360;
 
 void _sbus_telemetry_c(void) {}
 static unsigned char sbus_slot_data[31][3];
-const char sbus_slot_id[] = { 0x03, 0x83, 0x43, 0xc3, 0x23, 0xa3, 0x63, 0xe3,
-			 0x13, 0x93, 0x53, 0xd3, 0x33, 0xb3, 0x73, 0xf3,
-			 0x0b, 0x8b, 0x4b, 0xcb, 0x2b, 0xab, 0x6b, 0xeb,
-			 0x1b, 0x9b, 0x5b, 0xdb, 0x3b, 0xbb, 0x7b, 0xfb };
+
+/**
+ * Futaba Sbus2 slot numbering and frame aligement
+ * 	04h> Frame0				14h> Frame1			 	24h> Frame2				34h> Frame3
+ *	03h	00000011	Slot0	13h	00010011	Slot8	0Bh	00001011	Slot16	1Bh	00011011	Slot24
+ *	83h	10000011	Slot1	93h	10010011	Slot9	8Bh	10001011	Slot17	9Bh	10011011	Slot25
+ *	43h	01000011	Slot2	53h	01010011	Slot10	4Bh	01001011	Slot18	5Bh	01011011	Slot26
+ *	C3h	11000011	Slot3	D3h	11010011	Slot11	CBh	11001011	Slot19	DBh	11011011	Slot27
+ *	23h	00100011	Slot4	33h	00110011	Slot12	2Bh	00101011	Slot20	3Bh	00111011	Slot28
+ *	A3h	10100011	Slot5	B3h	10110011	Slot13	ABh	10101011	Slot21	BBh	10111011	Slot29
+ *	63h	01100011	Slot6	73h	01110011	Slot14	6Bh	01101011	Slot22	7Bh	01111011	Slot30
+ *	E3h	11100011	Slot7	F3h	11110011	Slot15	EBh	11101011	Slot23	FBh	11111011	Slot31
+ */
+
+const char sbus_slot_id[] = {
+				0x03, 0x83, 0x43, 0xc3, 0x23, 0xa3, 0x63, 0xe3,	// Frame0 04, Slot  0-7
+				0x13, 0x93, 0x53, 0xd3, 0x33, 0xb3, 0x73, 0xf3, // Frame1 14, Slot  8-15
+				0x0b, 0x8b, 0x4b, 0xcb, 0x2b, 0xab, 0x6b, 0xeb,	// Frame2 24, Slot 16-23
+				0x1b, 0x9b, 0x5b, 0xdb, 0x3b, 0xbb, 0x7b, 0xfb 	// Frame3 34, Slot 24-31
+			};
 static volatile char sbus_slot_index;
+//~ static int sbus_slot_index;
 static char sbus_slot_base;
 
-//~ extern void initialise_monitor_handles(void);
-
+#ifdef DEBUG
+extern void initialise_monitor_handles(void);
+#endif
 
 void init_clock(void)
 {
@@ -34,6 +59,7 @@ void init_gpio(void)
 	// LED0 PB3
 	rcc_periph_clock_enable(RCC_GPIOB);
 	gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED0);
+	gpio_set(GPIOB, LED0);
 }
 
 void init_usart(void)
@@ -53,18 +79,13 @@ void init_usart(void)
 	rcc_periph_clock_enable(RCC_GPIOA);
 	rcc_periph_clock_enable(RCC_USART1);
 
-	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9 | GPIO10);
-	gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO9 | GPIO10); // GPIO_OTYPE_PP tx ???
-	gpio_set_af(GPIOA, GPIO_AF7, GPIO9 | GPIO10);
-	//~ gpio_set(GPIOA, GPIO9 | GPIO10);
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
+	gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO9);  // GPIO_OTYPE_PP tx
+	gpio_set_af(GPIOA, GPIO_AF7, GPIO9);
 
 	//~ gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO10);
-	//~ gpio_set_output_options(GPIOA, GPIO_OTYPE_OD, GPIO_OSPEED_50MHZ, GPIO10); // GPIO_OTYPE_PP tx ???
-
-	//~ gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO10);
-	//~ gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO10); // GPIO_OTYPE_OD rx  ???
-	//~ gpio_set(GPIOA, GPIO10);
-	//~ gpio_set_af(GPIOA, GPIO_AF7, GPIO9);
+	//~ gpio_set_output_options(GPIOA, GPIO_OTYPE_OD, GPIO_OSPEED_50MHZ, GPIO10);
+	//~ gpio_set_af(GPIOA, GPIO_AF7, GPIO10);
 
 	usart_set_mode(SBUS_PORT, USART_MODE_TX_RX);
 	usart_set_baudrate(SBUS_PORT, 100000);
@@ -89,7 +110,6 @@ void init_usart(void)
 	//~ USART_CR3(SBUS_PORT) &= ~USART_CR3_SCEN;
 	//~ USART_CR3(SBUS_PORT) &= ~USART_CR3_IREN;
 
-
 	// disable
 	//~ USART_CR3(USART1) &= ~USART_CR3_OVRDIS;
 
@@ -112,27 +132,12 @@ void init_usart(void)
 
 void sbus_send_slot(int n)
 {
-	//~ prototype
 	if (sbus_slot_data[n][0] == 0) return;
-	//~ gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO10); // GPIO_OTYPE_PP tx ???
-	//~ gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO9);
-	//~ gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO9); // GPIO_OTYPE_PP tx ???
-	//~ gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_PULLDOWN, GPIO10);
 
-
-	//~ redefine gpio slot while sending data here
 	for (int i = 0; i < 3; i++) {
-		//~ while ((USART_ISR(SBUS_PORT) & USART_SR_TXE) == 0);
 		usart_send_blocking(SBUS_PORT, sbus_slot_data[n][i]);
-		usart_send_blocking(CONSOLE_PORT, sbus_slot_data[n][i]);
+		//~ usart_send_blocking(CONSOLE_PORT, sbus_slot_data[n][i]);
 	}
-
-	//~ gpio_set_output_options(GPIOA, GPIO_OTYPE_OD, GPIO_OSPEED_50MHZ, GPIO10); // GPIO_OTYPE_PP tx ???
-	//~ gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO9);
-	//~ gpio_set_output_options(GPIOA, GPIO_OTYPE_OD, GPIO_OSPEED_50MHZ, GPIO9); // GPIO_OTYPE_PP tx ???
-	//~ gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO10);
-	//~ while ((USART_ISR(SBUS_PORT) & USART_ISR_TC) == 0);
-	//~ redefine gpio back
 }
 
 
@@ -151,6 +156,7 @@ void init_timer(void)
 	rcc_periph_reset_pulse(RST_TIM2);
 	rcc_periph_clock_enable(RCC_TIM2);
 	TIM2_PSC = rcc_apb1_frequency / 500000; /* 1usec per count */
+	//~ TIM2_PSC = rcc_apb1_frequency / 500000 / 2 - 1;
 	TIM2_CR1 |= TIM_CR1_URS; /* setting EGR_UG won't trigger ISR */
 	TIM2_EGR |= TIM_EGR_UG; /* force load PSC */
 
@@ -161,6 +167,7 @@ void init_timer(void)
 	nvic_enable_irq(NVIC_TIM3_IRQ);
 
 	TIM3_PSC = rcc_apb1_frequency / 500000; /* 1usec per count */
+	//~ TIM3_PSC = rcc_apb1_frequency / 500000 / 2 - 1;
 	TIM3_ARR = 660; /* 3 bytes * 120us per byte  = 360 us for data
 			   an 300us gap between slots */
 	TIM3_DIER |= TIM_DIER_UIE;
@@ -183,27 +190,37 @@ void tim2_us(unsigned delay) // from jitel project
 
 static void sbus_slot(int n, unsigned short val)
 {
+	/**
+	 * example
+	 * val = 0x8182
+	 * val[2] & 0xff = 82
+	 * val[1] >> 8 = 81
+	*/
+
 	sbus_slot_data[n][0] = sbus_slot_id[n];
 	sbus_slot_data[n][2] = val & 0xff;
 	sbus_slot_data[n][1] = val >> 8;
+	//~ usart_send_blocking(CONSOLE_PORT, sbus_slot_data[n][0]);
+	//~ usart_send_blocking(CONSOLE_PORT, sbus_slot_data[n][1]);
+	//~ usart_send_blocking(CONSOLE_PORT, sbus_slot_data[n][2]);
+
 }
 
-static uint16_t swap(uint16_t a)
-{
-	return (a << 8) | (a >> 8);
-}
+//~ static uint16_t swap(uint16_t a)
+//~ {
+	//~ return (a << 8) | (a >> 8);
+//~ }
 
 static void sbus_update_slot_data()
 {
-	int rpm=100;
-	/* SBS-01RM */
-	rpm++;
-	sbus_slot(4, swap(rpm/6)); //rpm sensor
-	/* SBS-01V */
-	sbus_slot(5, 0x1000);
-	sbus_slot(6, 0xF0);
+	//~ /* SBS-01RM */
+	//~ rpm++;
+	//~ sbus_slot(4, swap(rpm/6)); //rpm sensor
+	//~ /* SBS-01V */
+	//~ sbus_slot(5, 0x1000);
+	//~ sbus_slot(6, 0xF0);
 	// temp
-	sbus_slot(1, 0x8181);
+	sbus_slot(1, 0x8180);
 }
 
 
@@ -214,13 +231,12 @@ void sbus_tick(void)
 
 	do {
 		buf[0] = usart_recv_blocking(SBUS_PORT);
-		//~ usart_send_blocking(CONSOLE_PORT, buf[0]);
-		//~ printf("%i \r\n",buf[0]);
 	}
 	while (buf[0] != 0x0f);
 
-	//~ printf("got 0xf\r\n");
-	led0_toggle;
+	#ifdef DEBUG
+	printf("got 0xf\r\n");
+	#endif
 
 	/** channel data is 25 bytes long,
 	 * 1 start bit + 8 data bit + parity bit + 2 stop bit
@@ -232,19 +248,24 @@ void sbus_tick(void)
 				     //~ USART_SR_NE | USART_SR_FE |
 				     //~ USART_SR_PE)) == 0 &&
 
-		//~ while ((USART_ISR(SBUS_PORT) & USART_ISR_RXNE) == 0 &&
-			//~ (TIM2_SR & TIM_SR_UIF) == 0);
+		while ((USART_ISR(SBUS_PORT) & (USART_SR_RXNE | USART_SR_ORE |
+				     USART_SR_NE | USART_SR_FE |
+				     USART_SR_PE)) == 0 &&
+		       (TIM2_SR & TIM_SR_UIF) == 0);
 
-		while ((TIM2_SR & TIM_SR_UIF) == 0);
+		//~ while (!(TIM2_SR & TIM_SR_UIF));
 
 		//~ if ((USART_ISR(SBUS_PORT) & (USART_SR_ORE | USART_SR_NE |
-				  //~ USART_SR_FE | USART_SR_PE)) ||
+			//~ USART_SR_FE | USART_SR_PE)) ||
+			//~ (TIM2_SR & TIM_SR_UIF))
+				//~ return;
+
 		buf[i] = usart_recv_blocking(SBUS_PORT);
 	}
 
 	//~ printf("%i %i %i\r\n", buf[2], buf[3], buf[24]);
 	//~ usart_send_blocking(CONSOLE_PORT, buf[24]);
-	led0_toggle;
+	//~ led0_toggle;
 	switch (buf[24]) {
 	case 0x04: sbus_slot_base = 0; break;
 	case 0x14: sbus_slot_base = 8; break;
@@ -262,6 +283,9 @@ void sbus_tick(void)
 	TIM3_CNT = 0;
 	TIM3_CR1 |= TIM_CR1_CEN; /* start slot timer */
 
+			//~ usart_send_blocking(CONSOLE_PORT, sbus_slot_base);
+			usart_send_blocking(CONSOLE_PORT, sbus_slot_index);
+
 	sbus_send_slot(sbus_slot_base + sbus_slot_index);
 	while (sbus_slot_index != 7); /* wait till all slots sent */
 }
@@ -275,8 +299,6 @@ int main(void)
 	init_gpio();
 	init_usart();
 	init_timer();
-	//~ usart_send_blocking(SBUS_PORT,255);
-	usart_send_blocking(CONSOLE_PORT,255);
 	//~ for(;;) 	usart_send_blocking(CONSOLE_PORT,18);
 	for (;;) {
 		sbus_tick();
